9ab95bc8a344718b7b9d04cc81dd0f5443392696
Algebra-FUN <algebra-fun@outlook.com>
2023-05-11 11:06:27 +0800

    [mod] use DSP conv

--- Project/1/code/FFTWrapper.jl
+++ Project/1/code/FFTWrapper.jl
@@ -1,8 +1,8 @@
 """
-This file contains a wrapper for the FFTW library.
+This file contains a wrapper for the FFTW and DSP library.
 """
 
-import FFTW
+import FFTW, DSP
 
 fftfreq = FFTW.fftfreq
 
@@ -11,5 +11,5 @@ fftemplate(N) = zero(FFTW.fftfreq(N))
 fft(x) = FFTW.fft(x) / length(x)
 ifft(x) = FFTW.ifft(x) * length(x)
 
-conv(f, g) = ifft(fft(f) .* fft(g))
+conv(f, g) = DSP.conv(f, g)[1:length(f)]
 conv(f) = conv(f, f)
