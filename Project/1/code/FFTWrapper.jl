29b89e50cc5604fbc3ca3a29ffa6384236255723
Algebra-FUN <algebra-fun@outlook.com>
2023-05-12 11:13:29 +0800

    [fix]

--- Project/1/code/FFTWrapper.jl
+++ Project/1/code/FFTWrapper.jl
@@ -8,8 +8,31 @@ fftfreq = FFTW.fftfreq
 
 fftemplate(N) = zero(FFTW.fftfreq(N))
 
-fft(x) = FFTW.fft(x) / length(x)
-ifft(x) = FFTW.ifft(x) * length(x)
+fftshift(x) = FFTW.fftshift(x)
+ifftshift(x) = FFTW.ifftshift(x)
 
-conv(f, g) = ifft(fft(f) .* fft(g))
+fftfreq = fftshift ∘ FFTW.fftfreq
+
+fft(x) = fftshift(FFTW.fft(x)) / length(x)
+ifft(x) = FFTW.ifft(ifftshift(x)) * length(x)
+
+# fftconv(f, g) = FFTW.ifft(FFTW.fft(f) .* FFTW.fft(g))
+# fftconv(f, g) = DSP.conv(f,g)[1:length(f)]
+
+function conv(f, g, method=:zero_padding)
+    h = zero(f)
+    N = length(g)
+    if method == :zero_padding
+        for k in eachindex(h)
+            h[k] = sum(m -> 0 <= k - m < N ? f[m] * g[k-m+1] : 0, eachindex(f))
+        end
+        return h
+    elseif method == :circular
+        for k in eachindex(h)
+            h[k] = sum(m -> f[m] * g[mod(k-m,N)+1], eachindex(f))
+        end
+        return h
+    end
+    throw(ArgumentError("Invalid method"))
+end
 conv(f) = conv(f, f)
